a69457c1fa5ad0f51848b6b63a66a4ec5558c1cf
fde-capu <flavio@carrara.us>
2022-02-09 21:56:19 +0100

    html4242

---
+++ general_tests.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -x
+
+divider()
+{
+	{ set +x; } 2> /dev/null
+	echo -e '\n\n--------------------------\n\n'
+	set -x
+}
+
+{ divider; } 2> /dev/null
+{ divider; } 2> /dev/null
+{ divider; } 2> /dev/null
+
+curl -vD- localhost:3490
+
+{ divider; } 2> /dev/null
+
+curl -vD- localhost:3491
+
+{ divider; } 2> /dev/null
+
+curl -vD- localhostB3491:3491
+
+{ divider; } 2> /dev/null
+
+curl -vD- localhost:3492
+
+{ divider; } 2> /dev/null
+
+curl -vD- localhost:3493
+
+{ divider; } 2> /dev/null
+
+curl -vD- localhost:3499 # does not exist
+
+{ divider; } 2> /dev/null
--- util_sh.md
+++ util_sh.md
@@ -4,3 +4,7 @@
 # `netstat -tunlp`
 ## nginx with specific `.conf`; `-t` for testing.
 # `nginx -c /home/coder/webserv/test-confs/nginx.conf -t`
+## curl showing everything about the negotiation and package (request and response headers):
+# `curl --verbose -D-` # or better: `-vD-`
+## nginx reload (uses the same configuration file previous given by -c)
+# `nginx -s reload`
