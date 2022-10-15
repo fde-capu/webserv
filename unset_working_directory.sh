ffc3a203a6891dc8e71f98f8bc0f377df01d04ff
fde-capu <flavio@carrara.us>
2022-10-16 01:20:03 +0200

    ...

--- unset_working_directory.sh
+++ unset_working_directory.sh
@@ -3,5 +3,5 @@
 MYSELF="$(realpath "$0")"
 MYDIR="${MYSELF%/*}"
 
-sed -i "s|${MYDIR}|WORKING_DIRECTORY|" ${MYDIR}/webserv-default.conf
-sed -i "s|${MYDIR}|WORKING_DIRECTORY|" ${MYDIR}/unit/webserv-unit.conf
+sed -i "s|^working_directory.*|working_directory WORKING_DIRECTORY/unit/confs|" ${MYDIR}/webserv-default.conf
+sed -i "s|^working_directory.*|working_directory WORKING_DIRECTORY/unit/confs|" ${MYDIR}/unit/webserv-unit.conf
